6efd44abfe0a3a12c0a45879f57af56d08564e00
Omar Gehad <omar.gehad593@gmail.com>
2025-01-31 23:48:24 +0200

    solving routing problems

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Header from "./Header/Header";
 import HomePage from "./HomePage/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import ServicesPage from "./ServicesPage/ServicesPage";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/services" element={<ServicesPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
